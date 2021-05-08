a06dbe36728a1d84b88edc9be9fc9e1c6e44b408
vdkhoa7498 <vdkhoa7498@gmail.com>
2021-05-08 21:03:50 +0700

    create new walet

---
+++ front_end/src/pages/createNewWallet/CreateNewWallet.jsx
@@ -0,0 +1,14 @@
+import {Button} from 'antd'
+
+const CreateNewWallet = (props) =>{
+
+    return(
+        <>
+            <Button>Create New Wallet</Button>
+            <Button>Create New Wallet</Button>
+        </>
+        
+    )
+}
+
+export default CreateNewWallet
